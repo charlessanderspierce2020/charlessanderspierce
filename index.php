229fedf001049bd6e3ff87376af52dfc98a7af92
charlessanderspierce2020 <65927954+charlessanderspierce2020@users.noreply.github.com>
2020-05-25 18:43:34 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-<?php require_once("res/x5engine.php"); ?>
 <!DOCTYPE html><!-- HTML5 -->
 <html prefix="og: http://ogp.me/ns#" lang="es-ES" dir="ltr">
 	<head>
-		<title>Buscar - CHARLES SANDERS PIERCE</title>
+		<title>CHARLES SANDERS PIERCE</title>
 		<meta charset="utf-8" />
 		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
 		<meta name="author" content="ANDRES " />
 		<meta name="generator" content="Incomedia WebSite X5 Professional 13.0.1.16 - www.websitex5.com" />
+		<meta name="description" content="Educativo" />
 		<meta property="og:locale" content="es" />
 		<meta property="og:type" content="website" />
-		<meta property="og:url" content="http://www.charlessanderspierce.com/imsearch.php" />
-		<meta property="og:title" content="Buscar" />
+		<meta property="og:url" content="http://www.charlessanderspierce.com/index.php" />
+		<meta property="og:title" content="INICIO" />
 		<meta property="og:site_name" content="CHARLES SANDERS PIERCE" />
 		<meta property="og:image" content="http://www.charlessanderspierce.com/favImage.png" />
 		<meta property="og:image:type" content="image/png">
@@ -23,7 +23,7 @@
 		<link rel="stylesheet" type="text/css" href="style/style.css?13-0-1-16" media="screen,print" />
 		<link rel="stylesheet" type="text/css" href="style/template.css?13-0-1-16" media="screen" />
 		<link rel="stylesheet" type="text/css" href="style/menu.css?13-0-1-16" media="screen" />
-		<link rel="stylesheet" type="text/css" href="pcss/imsearch.css?13-0-1-16-637260155628965566" media="screen" />
+		<link rel="stylesheet" type="text/css" href="pcss/index.css?13-0-1-16-637260155664910201" media="screen" />
 		<script type="text/javascript" src="res/jquery.js?13-0-1-16"></script>
 		<script type="text/javascript" src="res/x5engine.js?13-0-1-16" data-files-version="13-0-1-16"></script>
 		
@@ -32,14 +32,14 @@
 		<script type="text/javascript">
 			window.onload = function(){ checkBrowserCompatibility('El Explorador que estás usando no es compatible con las funciones requeridas para mostrar este Sitio web.','El Navegador que estás utilizando podría no ser compatible con las funciones requeridas para poder ver este Sitio web.','[1]Actualiza tu explorador [/1] o [2]continuar de todos modos[/2].','http://outdatedbrowser.com/'); };
 		</script>
-		<link rel="icon" href="favicon.png?13-0-1-16-637260155628965566" type="image/png" />
+		<link rel="icon" href="favicon.png?13-0-1-16-637260155654764729" type="image/png" />
 	</head>
 	<body>
 		<div id="imHeaderBg"></div>
 		<div id="imFooterBg"></div>
 		<div id="imPage">
 			<div id="imHeader">
-				<h1 class="imHidden">Buscar - CHARLES SANDERS PIERCE</h1>
+				<h1 class="imHidden">CHARLES SANDERS PIERCE</h1>
 				<div id="imHeaderObjects"><div id="imHeader_imObjectImage_04_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_4" class="" > <div id="imHeader_imCellStyleGraphics_4"></div><div id="imHeader_imCellStyle_4" ><img id="imHeader_imObjectImage_04" src="images/Logo.png" title="" alt="" /></div></div></div><div id="imHeader_imObjectTitle_05_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_5" class="" > <div id="imHeader_imCellStyleGraphics_5"></div><div id="imHeader_imCellStyle_5" ><div id="imHeader_imObjectTitle_05"><div id ="imHeader_imObjectTitle_05_text">CENTRO EDUCATIVO CHARLES SANDERS PIERCE </div > </div></div></div></div></div>
 			</div>
 			<a class="imHidden" href="#imGoToCont" title="Salta el menu principal">Vaya al Contenido</a>
@@ -49,7 +49,7 @@
 				<div id="imMnMn" class="auto main-menu">
 					<div class="hamburger-menu-background-container"><div class="hamburger-menu-background menu-mobile-hidden"><div class="hamburger-menu-close-button"><span>&times;</span></div></div></div>
 				<ul class="auto menu-mobile-hidden">
-					<li id="imMnMnNode0" class=" imPage">
+					<li id="imMnMnNode0" class="imPage imMnMnCurrent">
 						<a href="index.php">
 							<span class="imMnMnFirstBg">
 								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">INICIO</span></span>
@@ -92,18 +92,30 @@
 				<div id="imContentGraphics"></div>
 				<div id="imContent">
 					<a id="imGoToCont"></a>
-				<div id="imSearchPage">
-				<h2 id="imPgTitle">Resultados de búsqueda</h2>
-				<?php
-				$search = new imSearch();
-				$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search']) : "";
-				$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
-				$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
-				<div class="searchPageContainer">
-				<?php echo $search->search($keys, $page, $type); ?>
+				<div id="imGroup_1" class="imVGroup">
+				<div id="imPageRow_1" class="imPageRow">
+				
+				<div id="imPageRowContent_1" class="imContentDataContainer">
+				<div id="imGroup_2" class="imHGroup">
+				<div id="imCell_1" class="" > <div id="imCellStyleGraphics_1"></div><div id="imCellStyle_1" ><div id="imTextObject_01">
+					<div class="text-tab-content"  id="imTextObject_01_tab0" style="">
+						<div class="text-inner">
+							<div style="text-align: justify;"><div style="text-align: left;"><b><span class="fs24 cf1 ff1"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span class="fs24 cf2 ff1">sEMBRANDO</span></b></div><div style="text-align: left;"><span class="fs24 cf3 ff1"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span class="fs24 cf3 ff1"> </span><span class="fs24 ff1"> </span><span class="fs24 cf4 ff1">para</span></div><div style="text-align: left;"><span class="fs24 cf4 ff1"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;un futuro mejor</span></div></div><div style="text-align: justify;"><br></div><div style="text-align: justify;"><br></div><div style="text-align: justify;"><span class="fs12 cf5 ff2">NUESTRA IDEA ES FORMAR NIÑOS Y JÓVENES COMPROMETIDOS, RESPETUOSOS DE LA DIVERSIDAD DE PENSAMIENTO, ENTUSIASTAS DE LA LIBERTAD Y JUSTICIA FORTALECIDOS DE VALORES PATRIÓTICOS, DEMOCRÁTICOS, ESTUDIANTES CAPACES DE SUPERAR PROBLEMAS, SOLIDARIOS Y CON CREATIVIDAD.</span></div><div style="text-align: justify;"><br></div><div style="text-align: justify;"><span class="cf2 ff3"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="fs14">"EDUCAMOS CON AMOR PARA EL FUTURO"</span></span></div><div style="text-align: justify;"><br></div>
+						</div>
+					</div>
+				
 				</div>
+				</div></div><div id="imCell_7" class="" > <div id="imCellStyleGraphics_7"></div><div id="imCellStyle_7" ><a href="http://docs.google.com/forms/d/1q7u33rLUvBbJQ2nUsu1Q_EZ2x4jtqDtp9Ily46v8bFo/edit?usp=sharing" target="_blank"><img id="imObjectImage_07" src="images/matriculate-aqui-banner.png" title="" alt="" /></a></div></div>
+				</div></div>
+				</div>
+				<div id="imPageRow_2" class="imPageRow">
+				
+				<div id="imPageRowContent_2" class="imContentDataContainer">
+				<div id="imCell_6" class="" > <div id="imCellStyleGraphics_6"></div><div id="imCellStyle_6" ><div id="imObjectGallery_06"><div id="imObjectGalleryContainer_6"></div></div><script type="text/javascript">var imObjectGallery_06_settings = {'target': '#imObjectGallery_06','backgroundColor': 'transparent','description': { 'fontSize': '9pt','fontFamily': 'Tahoma','fontStyle': 'normal','fontWeight': 'normal'},'autoplay': true,'thumbs': { 'position': 'bottom', 'number': 7 },'controlBar': { 'show': true, 'color': '#141414' },'random': false,'fullScreen': false,'media': [{'type': 'image','width': 500,'height': 375,'url': 'gallery/imagen-1.png','thumb': 'gallery/imagen-1_thumb.png','autoplayTime': 5000,'effect': 'none'},{'type': 'image','width': 500,'height': 375,'url': 'gallery/imagen-4.png','thumb': 'gallery/imagen-4_thumb.png','autoplayTime': 5000,'effect': 'none'},{'type': 'image','width': 500,'height': 375,'url': 'gallery/imagen-5.png','thumb': 'gallery/imagen-5_thumb.png','autoplayTime': 5000,'effect': 'none'},{'type': 'image','width': 500,'height': 667,'url': 'gallery/imagen-8.png','thumb': 'gallery/imagen-8_thumb.png','autoplayTime': 5000,'effect': 'none'},{'type': 'image','width': 500,'height': 375,'url': 'gallery/imagen-10.png','thumb': 'gallery/imagen-10_thumb.png','autoplayTime': 5000,'effect': 'none'},{'type': 'image','width': 500,'height': 459,'url': 'gallery/4.png','thumb': 'gallery/4_thumb.png','autoplayTime': 5000,'effect': 'none'},{'type': 'image','width': 500,'height': 409,'url': 'gallery/1.png','thumb': 'gallery/1_thumb.png','autoplayTime': 5000,'effect': 'none'}]};var dataimObjectGallery_06 = null;function loadimObjectGallery_06() {var startIndex = 0;if(!!dataimObjectGallery_06) {imObjectGallery_06_settings.startFrom = dataimObjectGallery_06.currentItemIndex();}var w = 0;var h = 0;var cbp = x5engine.responsive.getCurrentBreakPoint();if (cbp.fluid) {w = $('#imObjectGallery_06').innerWidth();h = w / 500 * 375;imObjectGallery_06_settings.width = w;imObjectGallery_06_settings.height = h + (w / 7);imObjectGallery_06_settings.thumbs.size = (w / 7) - 2;}else if (cbp.hash == '6b0dc06cf1aa4f56df19794dd48b6ea7') {imObjectGallery_06_settings.width = 500;imObjectGallery_06_settings.height = 444;imObjectGallery_06_settings.thumbs.size = 69;}dataimObjectGallery_06 = x5engine.gallery(imObjectGallery_06_settings);$('#imContent').off('breakpointChangedOrFluid', loadimObjectGallery_06).on('breakpointChangedOrFluid', loadimObjectGallery_06);}x5engine.boot.push(loadimObjectGallery_06);</script></div></div></div>
 				</div>
 				
+				</div>
+				<div id="imFooPad"></div><div id="imBtMnContainer" class="imContentDataContainer"><div id="imBtMn"><a href="index.php">INICIO</a> | <a href="institucion.html">INSTITUCIÓN</a> | <a href="plataforma-digital.html">PLATAFORMA DIGITAL</a> | <a href="requisitos.html">REQUISITOS</a> | <a href="servicios.html">SERVICIOS</a> | <a href="contacto.html">CONTACTO</a> | <a href="imsitemap.html">Mapa general del sitio</a></div></div>
 					<div class="imClear"></div>
 				</div>
 			</div>
